6f40d144dc8300d2908a86908a8261e5e8e7ec30
Kris Sakarias <ksakarias@gmail.com>
2018-01-09 14:37:53 -0800

    added conditionals to respond to answers

--- js/app.js
+++ js/app.js
@@ -2,22 +2,58 @@
 
 confirm('This is a short quiz about me. Good luck!');
 
-var iceCream = prompt('Do I enjoy eating ice cream?');
-console.log('You answered ' + iceCream + '.');
-document.getElementById('iceCream').innerHTML = 'Your answer: ' + iceCream;
+var iceCream = prompt('Do I enjoy eating ice cream?').toLowerCase();
+if (iceCream === 'y' || iceCream === 'yes' || iceCream === 'n' || 
+    iceCream === 'no') {
+        console.log('You answered ' + iceCream + '.');
+        document.getElementById('iceCream').innerHTML = 'Your answer: ' + iceCream + 
+        '; ---> My answer: no';
+    } else {
+        console.log('You didn\'t answer yes or no.');
+        document.getElementById('iceCream').innerHTML = 'Your answer: n/a; ---> My answer: no';
+    }
 
-var pizza = prompt('Do I enjoy eating pizza?');
-console.log('You answered' + pizza + '.');
-document.getElementById('pizza').innerHTML = 'Your answer: ' + pizza;
 
-var brusselSprouts = prompt('Do I enjoy eating brussel sprouts?');
-console.log('You answered ' + brusselSprouts + '.');
-document.getElementById('brusselSprouts').innerHTML = 'Your answer: ' + brusselSprouts;
+var pizza = prompt('Do I enjoy eating pizza?').toLowerCase();
+if (pizza === 'y' || pizza === 'yes' || pizza === 'n' || 
+    pizza === 'no') {
+        console.log('You answered ' + pizza + '.');
+        document.getElementById('pizza').innerHTML = 'Your answer: ' + pizza + 
+        '; ---> My answer: yes';
+    } else {
+        console.log('You didn\'t answer yes or no.');
+        document.getElementById('pizza').innerHTML = 'Your answer: n/a; ---> My answer: yes';
+    }
 
-var cake = prompt('Do enjoy eating cake?');
-console.log('You answered ' + cake + '.');
-document.getElementById('cake').innerHTML = 'Your answer: ' + cake;
+var brusselSprouts = prompt('Do I enjoy eating brussel sprouts?').toLowerCase();
+if (brusselSprouts === 'y' || brusselSprouts === 'yes' || brusselSprouts === 'n' || 
+    brusselSprouts === 'no') {
+        console.log('You answered ' + brusselSprouts + '.');
+        document.getElementById('brusselSprouts').innerHTML = 'Your answer: ' + 
+        brusselSprouts + '; ---> My answer: yes';
+    } else {
+        console.log('You didn\'t answer yes or no.');
+        document.getElementById('brusselSprouts').innerHTML = 'Your answer: n/a; ---> My answer: yes';
+    }
 
-var eating = prompt('Do I enjoy eating?');
-console.log('You answered ' + eating + '.');
-document.getElementById('eating').innerHTML = 'Your answer: ' + eating;
+var cake = prompt('Do enjoy eating cake?').toLowerCase();
+if (cake === 'y' || cake === 'yes' || cake === 'n' || 
+    cake === 'no') {
+        console.log('You answered ' + cake + '.');
+        document.getElementById('cake').innerHTML = 'Your answer: ' + cake + 
+        '; ---> My answer: no';
+    } else {
+        console.log('You didn\'t answer yes or no.');
+        document.getElementById('cake').innerHTML = 'Your answer: n/a; ---> My answer: no';
+    }
+
+var eating = prompt('Do I enjoy eating?').toLowerCase();
+if (eating === 'y' || eating === 'yes' || eating === 'n' || 
+    eating === 'no') {
+        console.log('You answered ' + eating + '.');
+        document.getElementById('eating').innerHTML = 'Your answer: ' + eating + 
+        '; ---> My answer: yes';
+    } else {
+        console.log('You didn\'t answer yes or no.');
+        document.getElementById('eating').innerHTML = 'Your answer: n/a; ---> My answer: yes';
+    }
